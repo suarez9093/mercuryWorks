a003bf8b557e60fc5240d96d968763b3deff07ef
suarez9093 <suarez9093@hotmail.com>
2021-02-17 13:41:16 -0500

    added markup and class names

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,3 @@
+// Variables
+// =============================================================
 const url = 'https://official-joke-api.appspot.com/jokes/random';
-
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
